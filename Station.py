83dd72b45c206f2d65d3ef2e88af745b8d4a472c
Sean Selzer <seanselzer@googlemail.com>
2020-03-13 16:00:57 +0000

    Update repr on Station.py

--- Station.py
+++ Station.py
@@ -10,9 +10,12 @@ class Station:
     
     def __init__(self, name):
         self.name = name
-#        self.score = 30
-#        self.crew_list = []
-#        self.number_of_crew = len(self.crew_list)
+        self.score = 30
+        self.crew_list = []
+        self.number_of_crew = len(self.crew_list)
         
-#    def __str__(self):
-#        return "This is the {} station".format(self.name)
+    def __str__(self):
+        return "This is the {} station".format(self.name)
+    
+    def __repr__(self):
+        return "This is the {} station with the following {} people: {} and has score:{}".format(self.name, self.number_of_crew, self.crew_list, self.score)
