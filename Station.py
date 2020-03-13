65ec21a9e96fd827ab8a5c88a5ed06f8c4eb4b0a
Sean Selzer <seanselzer@googlemail.com>
2020-03-13 15:32:36 +0000

    Station.py v1.0

---
+++ Station.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 15:11:10 2020
+
+@author: seanselzer
+"""
+
+class Station:
+    
+    def __init__(self, name):
+        self.name = name
+        self.score = 30
+        self.crew_list = []
+        self.number_of_crew = len(self.crew_list)
+        
+#    def __str__(self):
+#        return "This is the {} station".format(self.name)
